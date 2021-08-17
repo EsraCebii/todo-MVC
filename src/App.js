dc94d3b2adb4948bc34d2ed464aeab960596a219
Esra Cebi <esracebi.81@gmail.com>
2021-08-17 19:12:33 +0300

    filters uncompleted.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
+
+import React,{ useState}from 'react';
 import './App.css';
 
+const INITIAL_STATE = [
+  { id: 1, baslik: "Learn React", tamamlandi: false },
+  { id: 2, baslik: "Learn JS", tamamlandi: false }
+];
+
 function App() {
+  const [liste, setListe] = useState(INITIAL_STATE);
+  const [yeniBaslik,setYeniBaslik]=useState("");
+  console.log("YENİ BAŞLIK:", yeniBaslik);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="todoapp">
+	    <header className="header">
+		    <h1>todos</h1>
+		    <form 
+          onSubmit={(e)=> 
+            
+            {
+              e.preventDefault();
+              setListe([
+                ...liste,
+                {id:Date.now(), baslik:yeniBaslik, tamamlandi:false }
+              ]);
+              setYeniBaslik("");
+            }
+
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+			    <input 
+            className="new-todo" 
+            value={yeniBaslik} 
+            placeholder="What needs to be done?" autoFocus 
+            onChange={(e)=>setYeniBaslik(e.target.value)} 
+          />
+		    </form>
+    	</header>
+
+	    <section className="main">
+		    <input className="toggle-all" type="checkbox" />
+		    <label htmlFor="toggle-all">
+			  Mark all as complete
+		    </label>
+		    <ul className="todo-list">
+          {liste.map(item=>(
+            <li className={item.tamamlandi ? 'completed' : ""} key={item.id}>
+              <div className="view" >
+                <input className="toggle" type="checkbox" onClick={()=>{
+                  setListe(
+                    liste.map(el => el.id=== item.id ? {...el, tamamlandi: !el.tamamlandi} : el
+                      ))}
+                  } />
+					      <label>{item.baslik}</label>
+					      <button className="destroy" onClick={(e)=> {
+                  setListe(liste.filter(el=> el.id !==item.id))} }/>
+              </div>
+            </li>
+          ))}
+		    </ul>
+	    </section>
+      <footer className="footer">
+        <span className="todo-count">
+          <strong>{liste.length} </strong>
+        items left
+        </span>
+
+        <ul className="filters">
+        <li>
+          <button onClick={()=> liste}>All</button>
+        </li>
+        <li>
+          <button onClick={()=> setListe(liste.filter(item=> !item.tamamlandi))}>Active</button>
+        </li>
+        <li>
+          <button onClick={()=> setListe(liste.filter(item=> item.tamamlandi))}>Completed</button>
+        </li>
+        </ul>
+
+        <button 
+          className="clear-completed" 
+          onClick={()=> setListe(liste.filter(item=> !item.tamamlandi))}
+        >
+        Clear completed
+        </button>
+      </footer>
+
+    </section>
+
   );
 }
 
